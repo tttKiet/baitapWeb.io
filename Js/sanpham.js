3bad9ed0f7182e0c24792404952c55904cb3cbdf
BuiKiet <roiyano1utl@gmail.com>
2022-11-26 23:07:10 +0700

    6. sanpham1

--- Js/sanpham.js
+++ Js/sanpham.js
@@ -2,9 +2,16 @@ const productMain = () => {
   //Handle search
   const searchIcon = $(".search-icon");
   const search = $(".search");
+  const rmIcon = $(".rm-icon");
   const groupSearch = $(".group-search");
   searchIcon.onclick = () => {
     groupSearch.style.display = "flex";
+    searchIcon.style.display = "none";
+  };
+
+  rmIcon.onclick = () => {
+    searchIcon.style.display = "block";
+    groupSearch.style.display = "none";
   };
 };
 
