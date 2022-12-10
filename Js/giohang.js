677b6a8ce33ecbe221c02f3ae8937a30b89443a6
BuiKiet <roiyano1utl@gmail.com>
2022-12-10 23:57:35 +0700

    20

--- Js/giohang.js
+++ Js/giohang.js
@@ -9,27 +9,63 @@ const addShop = $(".add-shop");
 
 const render = () => {
   let store = window.localStorage.getItem("store");
-  store = JSON.parse(store);
-  let itemStore = initProduct.productStore.filter((item) => {
-    return store.indexOf(`${item.id}`) > -1;
-  });
-  addShop.innerHTML = Carts(itemStore);
+  if (store) {
+    store = JSON.parse(store);
+    let itemStore = initProduct.productStore.filter((item) => {
+      return store.indexOf(`${item.id}`) > -1;
+    });
+    addShop.innerHTML = Carts(itemStore);
+  }
+};
+
+// Check xem trong localStorage đã có inputValue hay chưa
+const checkInputValue = () => {
+  const CheckValue = localStorage.getItem("inputValue");
+  if (CheckValue == null) {
+    localStorage.setItem("inputValue", JSON.stringify(valueInput));
+  }
+};
+
+// Xoá các id sản phẩm bị lặp lại
+const deleteIdProduct = () => {
+  const getStore = localStorage.getItem("store");
+  if (getStore) {
+    const data = JSON.parse(getStore);
+    data.sort();
+    for (let i = 0; i < data.length; i++) {
+      for (let j = i + 1; j < data.length; j++) {
+        if (data[j] == data[i]) {
+          data.splice(j, 1);
+          j--;
+        }
+      }
+    }
+    const dataChange = JSON.stringify(data);
+    localStorage.setItem("store", dataChange);
+  }
 };
 
 // Xoá sản phẩm
 const deleteItem = () => {
   const deleteIc = $$(".delete-icon");
   const getStore = localStorage.getItem("store");
-  const data = JSON.parse(getStore);
-  data.sort();
-  deleteIc.forEach((i, index) => {
-    i.addEventListener("click", () => {
-      data.splice(index, 1);
-      const dataChange = JSON.stringify(data);
-      localStorage.setItem("store", dataChange);
-      location.reload();
+  if (getStore) {
+    const data = JSON.parse(getStore);
+    data.sort();
+    deleteIc.forEach((i, index) => {
+      i.addEventListener("click", () => {
+        data.splice(index, 1);
+        const dataChange = JSON.stringify(data);
+        localStorage.setItem("store", dataChange);
+        const getDataInput = JSON.parse(localStorage.getItem("inputValue"));
+        getDataInput.splice(index, 1);
+        console.log(getDataInput);
+        localStorage.removeItem("inputValue");
+        localStorage.setItem("inputValue", JSON.stringify(getDataInput));
+        location.reload();
+      });
     });
-  });
+  }
 };
 
 // Tính tổng số sản phẩm
@@ -45,10 +81,12 @@ function updateTotalItem(arr) {
 // Sắp xếp thứ tự mảng trong localStorage
 function sortArray() {
   const getStore = localStorage.getItem("store");
-  const data = JSON.parse(getStore);
-  data.sort();
-  const dataChange = JSON.stringify(data);
-  localStorage.setItem("store", dataChange);
+  if (getStore) {
+    const data = JSON.parse(getStore);
+    data.sort();
+    const dataChange = JSON.stringify(data);
+    localStorage.setItem("store", dataChange);
+  }
 }
 
 // Đếm số lượng sản phẩm
@@ -100,6 +138,8 @@ const sumProduct = () => {
 const setQuantity = () => {
   const sumQT = $(".sum-quantity-total");
   sumQT.innerHTML = total;
+  const totalModal = $(".total-modal");
+  totalModal.innerHTML = total;
 };
 
 // Hàm chuyển number sang kiểu tiền tệ
@@ -112,6 +152,8 @@ const setSumPrice = () => {
   const sumTotal = $(".sum-total");
   var totalMoneyCur = convertMoney(totalMoney);
   sumTotal.innerHTML = totalMoneyCur;
+  const priceModal = $(".price-modal");
+  priceModal.innerHTML = totalMoneyCur;
 };
 
 // Chuyển number cột giá thành dạng tiền tệ
@@ -125,20 +167,33 @@ const convertMoneyDisplay = () => {
 
 // Lưu giá trị input vào localStorage
 let valueInput = [];
-console.log("asdads");
+
 const saveInputValue = () => {
+  const storeLength = JSON.parse(localStorage.getItem("store"));
   const inputBox = $$("#input");
-  sortArray();
-  var currentValueInput = JSON.parse(localStorage.getItem("inputValue"));
-  valueInput = currentValueInput;
-  console.log(currentValueInput);
+  if (storeLength) {
+    sortArray();
+    for (let i = 0; i < storeLength.length; i++) {
+      valueInput[i] = "1";
+    }
+    const getDataValue = JSON.parse(localStorage.getItem("inputValue"));
+    if (getDataValue.length == 0) {
+      localStorage.setItem("inputValue", JSON.stringify(valueInput));
+    }
+  }
+
   inputBox.forEach((i, index) => {
     i.addEventListener("change", () => {
-      valueInput.splice(index, 1, i.value);
-      console.log(valueInput);
-      let inputValueC = JSON.stringify(valueInput);
-      localStorage.setItem("inputValue", inputValueC);
-      console.log("asdads");
+      const valueInputClone = JSON.parse(localStorage.getItem("inputValue"));
+      if (valueInputClone) {
+        console.log(valueInputClone);
+        console.log(index);
+        const getArray = [...valueInputClone];
+        getArray[index] = i.value;
+        console.log(getArray);
+        localStorage.removeItem("inputValue");
+        localStorage.setItem("inputValue", JSON.stringify(getArray));
+      }
     });
   });
 };
@@ -149,6 +204,7 @@ const setValueInput = () => {
   window.addEventListener("load", () => {
     const dataInputStorage = localStorage.getItem("inputValue");
     const dataInput = JSON.parse(dataInputStorage);
+    console.log(dataInput);
     for (let i = 0; i < dataInput.length; i++) {
       if (inputBox[i].value == null) {
         inputBox[i].value = 1;
@@ -164,6 +220,8 @@ const setValueInput = () => {
 
 const cartMain = () => {
   render();
+  checkInputValue();
+  deleteIdProduct();
   deleteItem();
   countQuantity();
   sumProduct();
@@ -176,6 +234,7 @@ const cartMain = () => {
 
 cartMain();
 
+// Xử lý js phần modal
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
@@ -188,8 +247,42 @@ span.onclick = function () {
   modal.style.display = "none";
 };
 
+var modalSucess = document.getElementById("modal-success");
+var btnSuccess = document.getElementById("btn-success");
+var spanSuc = document.getElementsByClassName("close")[1];
+const inputAddress = $(".modal-input-address");
+const inputPhone = $(".modal-input-phone");
+const er1 = $(".er1");
+const er2 = $(".er2");
+// Check ô input
+btnSuccess.onclick = function () {
+  if (inputAddress.value.length == 0) {
+    er1.innerText = "Bạn chưa nhập địa chỉ.";
+    if (inputPhone.value.length == 0) {
+      er2.innerText = "Bạn chưa nhập số điện thoại.";
+    }
+
+    return;
+  } else {
+    er1.innerText = "";
+  }
+
+  if (inputPhone.value.length != 10) {
+    er2.innerText = "Số điện thoại phải là 10 số.";
+    return;
+  } else {
+    er2.innerText = "";
+  }
+  modalSucess.style.display = "block";
+};
+
+spanSuc.onclick = function () {
+  modalSucess.style.display = "none";
+};
+
 window.onclick = function (event) {
-  if (event.target == modal) {
+  if (event.target == modalSucess) {
+    modalSucess.style.display = "none";
     modal.style.display = "none";
   }
 };
